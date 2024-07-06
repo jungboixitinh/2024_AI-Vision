f9c7ea17fd055081154f7a0d7d825cdac3631c18
jungboixitinh <halehoangbao@gmail.com>
2024-07-06 17:06:12 +0700

    update project 1

--- Project 1/camerawowo.py
+++ Project 1/camerawowo.py
@@ -1,10 +1,11 @@
 import cv2
 import pathlib
 
+# Load the pre-trained face detection model
 cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
-
 clf = cv2.CascadeClassifier(str(cascade_path))
 
+# Start the webcam
 camera = cv2.VideoCapture(0)
 
 while True:
@@ -12,7 +13,10 @@ while True:
     if not ret:
         break
 
+    # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Detect faces in the frame
     faces = clf.detectMultiScale(
         gray,
         scaleFactor=1.1,
@@ -21,12 +25,15 @@ while True:
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
+    # Draw rectangles around detected faces
     for (x, y, width, height) in faces:
         cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 255, 0), 2)
 
+    # Display the frame
     cv2.imshow("Faces", frame)
     if cv2.waitKey(1) == ord("q"):
         break
 
+# Release the camera and close all OpenCV windows
 camera.release()
 cv2.destroyAllWindows()
---
+++ Project 1/readme_1.md
@@ -0,0 +1,42 @@
+# Real-Time Face Detection using Phone Camera
+
+This project demonstrates a simple real-time face detection application using your phone's camera. It captures video from the phonecam, detects faces in real-time, and draws rectangles around detected faces.
+
+## Prerequisites
+
+- Python 3.x
+- OpenCV library
+- Camo
+- Phone's camera
+
+## Installation
+
+1. **Clone the Repository:**
+
+    ```bash
+    git clone https://github.com/jungboixitinh/2024_AI-Vision.git
+    cd Project 1
+    ```
+
+2. **Install Required Libraries:**
+
+    Ensure you have `opencv-python` installed. You can install it using pip:
+
+    ```bash
+    pip install opencv-python
+    ```
+
+## Usage
+
+1. **Run the Script:**
+
+    ```bash
+    python camerawowo.py
+    ```
+
+    This will start your cam and display a window showing the video feed with rectangles drawn around detected faces.
+
+2. **Quit the Application:**
+
+    Press the `q` key to quit the application and close the webcam feed.
+
